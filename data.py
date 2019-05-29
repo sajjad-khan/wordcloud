d128956c54a38b15e2c5cf6135a230c126eb001b
Sajjad Khan <sajjjad.khan@appsight.us>
2019-05-29 12:55:10 +0500

    initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A repellendus magnam quae ullam omnis voluptatibus veritatis temporibus molestiae. Dolore et illum, necessitatibus omnis dolorum optio quasi beatae temporibus voluptas? Aspernatur!',
+            'author': 'Sajjad Khan',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A repellendus magnam quae ullam omnis voluptatibus veritatis temporibus molestiae. Dolore et illum, necessitatibus omnis dolorum optio quasi beatae temporibus voluptas? Aspernatur!',
+            'author': 'Safeer Khan',
+            'create_date': '02-12-2001'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A repellendus magnam quae ullam omnis voluptatibus veritatis temporibus molestiae. Dolore et illum, necessitatibus omnis dolorum optio quasi beatae temporibus voluptas? Aspernatur!',
+            'author': 'Naseer Khan',
+            'create_date': '02-12-2021'
+        }
+    ]
+    return articles
